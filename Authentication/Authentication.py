239b5e29df533f8f5ab8e8637aff6cca6185f7e8
Tanmay Garg <xtanmaygarg@gmail.com>
2023-02-07 01:17:02 +0530

    Retrieve Auth Token from service account

--- Authentication/Authentication.py
+++ Authentication/Authentication.py
@@ -1,3 +1,6 @@
+import google.auth
+import google.auth.transport.requests
+from google.oauth2 import service_account
 from google.oauth2 import service_account
 from googleapiclient import discovery
 from google.cloud import storage as dns
@@ -7,11 +10,11 @@ project_id = 'vsa-dev-298916'
 
 
 gcp_service_account_json = None
-with open("/Users/tjaiswal/Downloads/vsa-dev-298916-7a558f236b34.json") as json_file:
+with open("/Users/tanmaygarg/Downloads/Account.json") as json_file:
     gcp_service_account_json = json.load(json_file)
 
 credentials = service_account.Credentials.from_service_account_info(
-    gcp_service_account_json)
-
-
-client = dns.Client(project=project_id, credentials=credentials)
+    gcp_service_account_json, scopes=['https://www.googleapis.com/auth/cloud-platform'])
+auth_req = google.auth.transport.requests.Request()
+credentials.refresh(auth_req)
+# print(credentials.token)
